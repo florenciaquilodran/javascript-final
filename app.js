12b5a3b841299e2a2ff72f90a9dbec610af9c36c
Florencia Quilodran <florenciaquilodran@gmail.com>
2022-06-06 19:17:41 -0300

    arreglé unos errores que no me funcionaban con el fetch y decidi crear una clase constructora

--- app.js
+++ app.js
@@ -8,68 +8,35 @@ Swal.fire({
     width: 600,
     padding: '3em',
     color: '#716add',
-    background: '#fff url(/images/trees.png)',  //Tanto esta imagen como la de abajo no me estarian funcionando pero no se el por que 
-    backdrop: `
-        rgba(0,0,123,0.4)
-        url("/images/nyan-cat.gif")             
-        left top
-        no-repeat
-    `
     })
 
 
 
 //Un array de signos
 
-const datosAstrológicos = [
-
-    {'signo': 'Aries', 
-        'dato': 'Es el fuego que inicia, que irrumpe, que quema pronto y pronto se apaga. Furia, ira, actividad, acción.'
-    },
-    {'signo': 'Tauro', 
-        'dato': 'La tranquilidad del hogar, mirar una pelicula en el sillón con una rica comida.'
-    },
-    {'signo': 'Géminis', 
-        'dato': 'Alegría, diversión, estudiar de manera constante, tener muchos amigos por todos lados.'
-    },
-    {'signo': 'Cáncer', 
-        'dato': 'La familia es lo primero, el cobijo fundamental, la Madre es lo mas importante.'
-    },
-    {'signo': 'Leo', 
-        'dato': 'Soy el lider, el rey de la manada, todos me siguen de manera espontanea.'
-    },
-    {'signo': 'Virgo', 
-        'dato': 'Necesito cuidar de manera meticulosa mi cuerpo, saber como actua.'
-    },
-    {'signo': 'Libra', 
-        'dato': 'Equilibrio entre ambas partes, sere el mediador.'
-    },
-    {'signo': 'Escorpio', 
-        'dato': 'Purgar. Sacar al exterior todo aquello que se pudre dentro mio. Renacer.'
-    },
-    {'signo': 'Sagitario', 
-        'dato': 'Viajar, explorar, divertirme, hablar otros idiomas, ser maestro, el sabio.'
-    },
-    {'signo': 'Capricornio', 
-        'dato': 'Trabajar dia y noche sin parar para lograr mis cometidos.'
-    },
-    {'signo': 'Piscis', 
-        'dato': 'Amor total para con toda la humanidad.'
-    },
-];
-
-function generarDatoAstrologico(){
-    const random = Number.parseInt(Math.random()*datosAstrológicos.length + 1);
-    document.querySelector('#quoteOutput').textContent = `\"${datosAstrológicos[random].dato}\"`;
-    document.querySelector('#authorOutput').textContent = `-${datosAstrológicos[random].signo}`;
-    
+class objetoSigno {
+    constructor(id, dato) {
+        this.id = id;
+        this.dato = dato;
+    };
 }
 
-console.log(...datosAstrológicos); // He aqui un SPREAD tal como enseñaron en clase , donde a través de la consola vamos a poder ver el array con dichos "datos astrologicos"
+const arraySignos = [
+    
 
-fetch("/javascript.json")
-.then( (response) => {
-    return response.json();
-}) .then( (generarDatoAstrologico) => (
-    console.log(generarDatoAstrologico)
-))
+]
+fetch("https://florenciaquilodran.github.io/javascript-final/javascript.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let entradasKeys = Object.keys(data);
+        let entradasValue = Object.values(data);
+        for (let i = 0; i < entradasKeys.length; i++) {
+            arraySignos.push(new objetoSigno(entradasKeys[i], entradasValue[i]));            
+        }
+    });
+
+            function generarDatoAstrologico() {
+                const random = Number.parseInt(Math.random() * arraySignos.length -1);
+                document.querySelector('#fraseOutput').textContent = `-${arraySignos[random].dato.dato}-`;
+                document.querySelector('#signoOutput').textContent = `-${arraySignos[random].dato.signo}-`;
+            }
